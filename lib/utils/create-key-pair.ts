78bdfc83869b25896c9da49ff0613ee67051d56b
Wizz Wallet <153743376+wizz-wallet-dev@users.noreply.github.com>
2024-10-15 08:42:00 +0800

    Fix ecc initialization

--- lib/utils/create-key-pair.ts
+++ lib/utils/create-key-pair.ts
@@ -1,8 +1,8 @@
 const bitcoin = require('bitcoinjs-lib');
+import * as ecc from 'tiny-secp256k1';
 bitcoin.initEccLib(ecc);
 
 import ECPairFactory from 'ecpair';
-import * as ecc from 'tiny-secp256k1';
 import { defaultDerivedPath } from './address-helpers';
 import { createMnemonicPhrase } from './create-mnemonic-phrase';
 
